61bb5a032b2a394665719074d1441ba98eaad6f5
github username <github email address>
2020-08-10 22:57:58 -0400

    updated vue app

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+console.log('[i] Starting client app')
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+//   store,
+//   router,
+//   components: { App }
+// }).$mount('#app')
 
 Vue.config.productionTip = false
 
@@ -10,3 +22,19 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+// new Vue({
+//   el: '#app',
+//   data () {
+//     return {
+//       info: null
+//     }
+//   },
+//   mounted () {
+//     axios
+//       .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+//       .then(response => (this.info = response))
+//   }
+// })
+
+
