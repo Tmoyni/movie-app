8998da388095e1e67826441de77c345e64532d5f
github username <github email address>
2020-08-16 23:46:42 -0400

    setting up components for movies

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MovieOne msg="Cast Compare"/>
+    <MovieOne msg="Cast Compare"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MovieOne from '@/components/MovieOne.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    MovieOne
   }
 }
 </script>
